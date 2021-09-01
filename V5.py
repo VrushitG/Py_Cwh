a35f49c5685450c98e0c195a44b9e47330c3c90e
Vrushit <vrushitgoyani01@gmail.com>
2021-09-01 23:50:38 +0530

    End Fun Use

--- V5.py
+++ V5.py
@@ -6,4 +6,9 @@ Pleas Dont remove this line
 this is multi -
 line comment
 """
-print("Hello World")
+print("Hello World""This is vrushit G")
+print("Hello World","This is vrushit G")  #this is for adding space betweent two statment
+
+print("Hello World", end=" Joker   ")    #end function for adding somthing in new line
+print("Hello World 2")
+
