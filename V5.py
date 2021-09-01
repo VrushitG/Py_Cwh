ae149be981491e19470a61178453512c22d6219a
Vrushit <vrushitgoyani01@gmail.com>
2021-09-02 00:02:57 +0530

    Escape Sequence Char

--- V5.py
+++ V5.py
@@ -12,3 +12,12 @@ print("Hello World","This is vrushit G")  #this is for adding space betweent two
 print("Hello World", end=" Joker   ")    #end function for adding somthing in new line
 print("Hello World 2")
 
+print("C:\harry")
+print("C:\narry")   #\n is consider as new line
+print("C:\\narry")  #this is called escap sequence character
+print("C:\"harry")  #for adding double quote sign
+print("C:\'harry\'") #for adding single quote sign
+
+print("This is \n good boy \t1") #\n for new line \t for one TAB space
+
+
