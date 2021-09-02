8a51166f555faa0fd9b126a757f80f951756bc47
Vrushit <vrushitgoyani01@gmail.com>
2021-09-02 22:41:10 +0530

    Making String into Intiger

--- Tut7.py
+++ Tut7.py
@@ -1,7 +1,7 @@
-var1 = "Hello Vrushit"
+var1 = "54"
 var2 = 5
 var3 = 36.5
-var4 = " Goyani"
+var4 = "32"
 print(var1,var3,var2)
 
 print(type(var1))  #type is for what type of variable is this.
@@ -11,3 +11,5 @@ print(type(var3))  #type is for what type of variable is this.
 print(var2 + var3) #also use as math.
 print(var1 + var4) #also use as Addig.
 print(type(var1))
+
+print(int(var1) + int(var4)) #MAke str into Int
