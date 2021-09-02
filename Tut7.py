dc4104105065fcbf1f0f272463964a80c85ce57a
Vrushit <vrushitgoyani01@gmail.com>
2021-09-02 22:57:01 +0530

    UserInput

--- Tut7.py
+++ Tut7.py
@@ -2,7 +2,7 @@ var1 = "54"
 var2 = 5
 var3 = 36.5
 var4 = "32"
-print(var1, var3, var2)
+"""print(var1, var3, var2)
 
 print(type(var1))  #type is for what type of variable is this.
 print(type(var2))  #type is for what type of variable is this.
@@ -16,3 +16,8 @@ print(int(var1) + int(var4))    #MAke str into Int
 
 print(10 * str(int(var1) + int(var4)))   #MAke str into Int
 print(10 * "Hello World\n")     #Print 10 times given string in new line THIS METHOD Called AS TYPE CASTING
+"""
+
+print("Enter Your number: ")
+user = input()
+print("You entered: ", user)
