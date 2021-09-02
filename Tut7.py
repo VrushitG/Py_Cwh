c4c5221d97eba95d36325cb6d56336c52b0fd6a7
Vrushit <vrushitgoyani01@gmail.com>
2021-09-02 22:51:45 +0530

    Type Casting

--- Tut7.py
+++ Tut7.py
@@ -2,7 +2,7 @@ var1 = "54"
 var2 = 5
 var3 = 36.5
 var4 = "32"
-print(var1,var3,var2)
+print(var1, var3, var2)
 
 print(type(var1))  #type is for what type of variable is this.
 print(type(var2))  #type is for what type of variable is this.
@@ -12,4 +12,7 @@ print(var2 + var3) #also use as math.
 print(var1 + var4) #also use as Addig.
 print(type(var1))
 
-print(int(var1) + int(var4)) #MAke str into Int
+print(int(var1) + int(var4))    #MAke str into Int
+
+print(10 * str(int(var1) + int(var4)))   #MAke str into Int
+print(10 * "Hello World\n")     #Print 10 times given string in new line THIS METHOD Called AS TYPE CASTING
