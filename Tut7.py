ac80f702f5bbecd303a53d4fc83aad4c2eaccec7
Vrushit <vrushitgoyani01@gmail.com>
2021-09-02 22:36:55 +0530

    Adding AS math

--- Tut7.py
+++ Tut7.py
@@ -1,8 +1,13 @@
-var1 = "Hello World"
+var1 = "Hello Vrushit"
 var2 = 5
 var3 = 36.5
+var4 = " Goyani"
 print(var1,var3,var2)
 
 print(type(var1))  #type is for what type of variable is this.
 print(type(var2))  #type is for what type of variable is this.
 print(type(var3))  #type is for what type of variable is this.
+
+print(var2 + var3) #also use as math.
+print(var1 + var4) #also use as Addig.
+print(type(var1))
