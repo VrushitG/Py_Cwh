c6fc26b64e001a3b475ebe9d7966e36df94dd162
Vrushit <vrushitgoyani01@gmail.com>
2021-09-02 22:32:30 +0530

    type Fun

--- Tut7.py
+++ Tut7.py
@@ -2,3 +2,7 @@ var1 = "Hello World"
 var2 = 5
 var3 = 36.5
 print(var1,var3,var2)
+
+print(type(var1))  #type is for what type of variable is this.
+print(type(var2))  #type is for what type of variable is this.
+print(type(var3))  #type is for what type of variable is this.
