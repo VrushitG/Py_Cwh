3988407779b5c60f6a9053096ff6b42b14e1af75
Vrushit <vrushitgoyani01@gmail.com>
2021-09-02 23:41:23 +0530

    str Funnn

--- tut8.py
+++ tut8.py
@@ -2,3 +2,12 @@ mystr = "Vrushit is good boy"
 print(mystr[3])
 print(len(mystr)) #for knowing the length of string
 print(mystr[0:3]) #for stating to given numebr
+print(mystr[0:7]) #for stating to given numebr
+print(mystr[0:7:2]) #last :2 is for take as 1 skip and take another
+print(mystr[0:])
+print(mystr[:5])
+print(mystr[::])
+print(mystr[::2])
+print(mystr[-1:])
+print(mystr[-4:-2])
+print(mystr[-4:-2:-1])
