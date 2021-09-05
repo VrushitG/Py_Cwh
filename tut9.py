60d67249f83903e9c3386a46db814c958158d2c0
Vrushit <vrushitgoyani01@gmail.com>
2021-09-05 16:07:38 +0530

    sort,revers

--- tut9.py
+++ tut9.py
@@ -1,2 +1,7 @@
 grocery = ["Harpic", "Vim bar", "DeoDrant", "Bhindi", "Lollypop", 56]
-print(grocery)
+print(grocery[2])
+
+numbers = [2, 7, 9, 11, 3, 0, 1, 14, 12]
+numbers.sort()
+numbers.reverse()
+print(numbers)
